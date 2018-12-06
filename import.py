e7e168d17ef26902d80b737e5ebdeed2e0b376bf
Sam Ou <lll810502@gmail.com>
2018-12-06 15:46:52 +0800

    hint the times

--- import.py
+++ import.py
@@ -1,14 +1,16 @@
 import random
 
 r = random.randint(1, 100)
-
+time = 0
 while True:
     game = input('猜數字遊戲！請輸入數字：')
     game = int(game)
+    time = time + 1
     if game == r:
         print('猜對囉66666')
         break
     elif game > r:
-        print('太大囉，猜小一點，你剛猜的是', game)
+        print('太大囉，猜小一點，你剛猜了第', time, '次', '是', game)
     elif game < r:
-        print('太小囉，猜大一點，你剛猜的是', game)
+        print('太小囉，猜大一點，你剛猜了第', time, '次', '是', game)
+
