f0adb01cead09f027964f8162329c9f65a2c7393
Sam Ou <lll810502@gmail.com>
2018-12-06 16:09:09 +0800

    fix times

--- import.py
+++ import.py
@@ -8,9 +8,11 @@ while True:
     time = time + 1
     if game == r:
         print('猜對囉66666')
+        print('你剛猜的', game, '是第', time, '次',)
         break
     elif game > r:
-        print('太大囉，猜小一點，你剛猜了第', time, '次', '是', game)
+        print('太大囉，猜小一點')
     elif game < r:
-        print('太小囉，猜大一點，你剛猜了第', time, '次', '是', game)
+        print('太小囉，猜大一點')
+    print('你剛猜的', game, '是第', time, '次',)
 
